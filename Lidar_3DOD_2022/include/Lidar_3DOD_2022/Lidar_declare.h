b7e24f770929f223bbe7577cc8b33049413533cf
Songhanbin <thdgksqls369@naver.com>
2022-04-28 01:55:05 +0900

    final_contest_code

---
+++ Lidar_3DOD_2022/include/Lidar_3DOD_2022/Lidar_declare.h
@@ -0,0 +1,132 @@
+#ifndef LIDAR_DECLARE
+#define LIDAR_DECLARE
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <time.h>
+#include <ros/ros.h>
+#include <boost/format.hpp>
+#include <std_msgs/String.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/common.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
+#include <pcl/features/integral_image_normal.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/statistical_outlier_removal.h>   //noise filtering
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/surface/mls.h>
+#include <pcl/surface/impl/mls.hpp>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <Lidar_3DOD_2022/SDW_DBSCAN.h>
+//#include <Lidar_process_div/DBSCAN_hanbin.h>
+//#include "Lidar_pkg/Lidar_msg.h"  //include "패키지 명/메시지 파일 명.h"
+
+using namespace std;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PCXYZ;
+typedef pcl::PointCloud<pcl::PointXYZI> PCXYZI;
+typedef pcl::PointXYZ PXYZ;
+typedef pcl::PointXYZI PXYZI;
+
+ros::Publisher pub_ROI;     //ROI
+ros::Publisher pub_Euclid;    //clustering1
+ros::Publisher pub_RS;      //ransac
+ros::Publisher pub_GND;     //ground
+ros::Publisher pub_DBscan;  //DBscan
+ros::Publisher pub_msg;
+//ros::Publisher OUT_MSG;     //out message
+
+double REMOVE_FACTOR;
+float voxel_size_x, voxel_size_y, voxel_size_z;
+float ROI_xMin, ROI_xMax, ROI_yMin, ROI_yMax, ROI_zMin, ROI_zMax;
+float Ransac_Z_ROI;
+float DBscan_eps;
+float DBscan_minPts;
+int DB_MinClusterSize, DB_MaxClusterSize;
+float EC_eps;
+int EC_MinClusterSize, EC_MaxClusterSize;
+double ransac_distanceThreshold; //함수 : double형 parameter
+bool switch_NoiseFiltering;
+bool switch_jiwon_filter;
+bool switch_DY_filter;
+bool switch_ROI;
+//bool switch_DownSampling;
+bool switch_Euclid;
+bool switch_UpSampling;
+bool switch_RanSaC;
+bool switch_DBscan;
+
+//func
+void ROI(const sensor_msgs::PointCloud2ConstPtr&);
+void makeCropBox (PCXYZI& Cloud, float xMin, float xMax, float yMin, float yMax, float zMin, float zMax);
+void UpSampling(PCXYZI&, PCXYZI::Ptr);
+void DownSampling(PCXYZI&, PCXYZI::Ptr);
+void NoiseFiltering(PCXYZI::Ptr, PCXYZI::Ptr);
+void DBScanClustering(PCXYZI::Ptr, PCXYZI&);
+void EuclideanClustering(PCXYZI::Ptr, PCXYZI&);
+void RanSaC(PCXYZI::Ptr);
+void normalEstimation (PCXYZI::Ptr cloud);
+string send_msg_DXY(PXYZI);
+string send_msg_minmax(float, float, float, float);
+string send_msg_cnt(int);
+void msg_process(vector<pair<PXYZI,string>>&);
+
+class Filter{
+public:
+    void DY_filter(vector<pair<PXYZI,string>>& sorted_OBJ, bool flag);
+    void jiwon_filter(vector<pair<PXYZI,string>>& sorted_OBJ, bool flag);
+};
+Filter FT;
+
+class Fps{
+public:
+    double prev_clock;
+    double cur_clock;
+    double interval;
+    double m_fps;
+    size_t m_count;
+
+    Fps();
+    // Update
+    void update();
+};
+Fps FPS1;
+
+inline float cal_dist(float x, float y){ return sqrt(x*x+y*y); }
+inline float MidPt(float a, float b){ return (a + b) / 2; }
+inline void print_coord(PXYZI tmp){ cout << fixed << setprecision(3) <<"dist : " << cal_dist(tmp.x,tmp.y) << "    x : "<<tmp.x << "    y : "<< tmp.y <<endl; }
+inline bool check_in(PXYZI a, PXYZI b) { return ((abs(a.x - b.x) <= REMOVE_FACTOR) && (abs(a.y - b.y) <= REMOVE_FACTOR)); }
+inline void print_OBJ(vector<pair<PXYZI,string>>& sorted_OBJ){ for(int i = 0; i < sorted_OBJ.size(); i++) print_coord(sorted_OBJ[i].first); }
+
+template<typename T> //this func is used all code
+void pub_process(T& input, sensor_msgs::PointCloud2& output){
+    pcl::PCLPointCloud2 tmp_PCL;                               //declare PCL_PC2
+    pcl::toPCLPointCloud2(input, tmp_PCL);                     //PC -> PCL_PC2
+    pcl_conversions::fromPCL(tmp_PCL, output);                 //PCL_PC2 -> sensor_msg_PC2
+    output.header.frame_id = "velodyne";
+}
+
+#endif
---
+++ Lidar_3DOD_2022/src/node_code/Lidar_Clustering.cpp
@@ -0,0 +1,57 @@
+#include <Lidar_3DOD_2022/Lidar_declare.h>
+
+using namespace std;
+
+void Clustering_process(const sensor_msgs::PointCloud2ConstPtr& aft_ransac){
+    PCXYZI tmp;
+    pcl::fromROSMsg(*aft_ransac,tmp);
+    PCXYZI::Ptr upsampledCloud (new PCXYZI);;
+    copyPointCloud(tmp,*upsampledCloud);
+    //-----------NoiseFiltering-----------
+    PCXYZI::Ptr filteredCloud (new PCXYZI); //saving spcae for filteredData
+    if( switch_NoiseFiltering ) NoiseFiltering( upsampledCloud, filteredCloud); //call NoiseFiltering func  ////  current : on
+    else filteredCloud = upsampledCloud;
+    //-----------clustering------------
+    PCXYZI Fin_Cloud;
+    if( switch_DBscan ) DBScanClustering( filteredCloud, Fin_Cloud); //prior DBSCAN
+    else if( switch_Euclid ) EuclideanClustering( filteredCloud, Fin_Cloud );
+    else{
+        Fin_Cloud = *filteredCloud;
+        sensor_msgs::PointCloud2 output; 
+        pub_process(Fin_Cloud,output);
+        pub_DBscan.publish(output);        
+    } 
+    FPS1.update();
+}
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "Clustering"); //node name 
+	ros::NodeHandle nh;         //nodehandle
+
+    nh.getParam("/Clustering_node/switch_NoiseFiltering", switch_NoiseFiltering);
+    //Euclid
+    nh.getParam("/Clustering_node/switch_Euclid", switch_Euclid);
+    nh.getParam("/Clustering_node/EC_eps", EC_eps);
+    nh.getParam("/Clustering_node/EC_MinClusterSize", EC_MinClusterSize);
+    nh.getParam("/Clustering_node/EC_MaxClusterSize", EC_MaxClusterSize);
+    //DBSCAN
+    nh.getParam("/Clustering_node/switch_DBscan", switch_DBscan);
+    nh.getParam("/Clustering_node/DBscan_eps", DBscan_eps);
+    nh.getParam("/Clustering_node/DBscan_minPts", DBscan_minPts);
+    nh.getParam("/Clustering_node/DB_MinClusterSize", DB_MinClusterSize);
+    nh.getParam("/Clustering_node/DB_MaxClusterSize", DB_MaxClusterSize);
+    //etc
+    nh.getParam("/Clustering_node/switch_jiwon_filter", switch_jiwon_filter);
+    nh.getParam("/Clustering_node/switch_DY_filter", switch_DY_filter);
+    nh.getParam("/Clustering_node/Ransac_Z_ROI", Ransac_Z_ROI);  //DY_filter param
+    nh.getParam("/Clustering_node/REMOVE_FACTOR", REMOVE_FACTOR);
+
+	ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/2_1_velodyne_points_ransac", 100, Clustering_process);
+    if( switch_Euclid && !switch_DBscan ) pub_Euclid = nh.advertise<sensor_msgs::PointCloud2> ("/3_velodyne_points_Euclid", 1);
+    else if( switch_DBscan ) pub_DBscan = nh.advertise<sensor_msgs::PointCloud2> ("/3_velodyne_points_DBSCAN", 1);
+    pub_msg = nh.advertise<std_msgs::String> ("/Lidar_msg",1); 
+
+    //OUT_MSG = nh.advertise<Lidar_pkg::Lidar_msg> ("/lidar_detected_object", 1);
+    //<패키지 명/메시지 파일 명>
+	ros::spin();
+}
